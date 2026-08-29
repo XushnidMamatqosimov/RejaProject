e11dc592b7939e4f74d1e4e84e939365720bd3f0
Xushnid <xmamtqosimov@gmail.com>
2026-08-29 20:35:29 +0900

    feat: train callback and async functions

--- train.js
+++ train.js
@@ -8,24 +8,68 @@ const list = [
     "endi damni oling xoji aka, " // 60 => 
 ];
 
-function maslahatBering(a, callback){
-    if(typeof a !== "number") callback("please enter a number", null);
+// CallBack
+async function maslahatBering(a, callback){
+    if(typeof a !== "number") callback("please enter number", null);
     else if ( a <= 20) callback(null, list[0]);
-    else if ( a > 20 && a<=30) callback(null, list[1]);
-    else if ( a > 30  && a<=40) callback(null, list[2]);
+    else if ( a > 20 && a<=30)  callback(null, list[1]);
+    else if ( a > 30  && a<=40)  callback(null, list[2]);
     else if ( a > 40 && a<=50) callback(null, list[3]);
-    else if ( a >50 && a<=60) callback(null, list[4]);
+    else if ( a >50 && a<=60)  callback(null, list[4]);
     else{
-        callback(null, list[5]);
+        setTimeout(()=> {
+            callback(null, list[5]);
+        }, 5000);
     }
-}
+} 
 
 console.log("passed here 0");
-maslahatBering("Salom", (err, data)=> {
-     if(err){
-        console.log("error:", err);
-     } else{
-        console.log("Javob: ", data);
-     }
+maslahatBering(12, (err, data)=>{
+    if(err) console.log("Error: ", err);
+    else{
+        console.log(data);
+    }
 });
 console.log("passed here 1");
+
+
+
+
+// Asynchronous
+/* async function maslahatBering(a){
+    if(typeof a !== "number") throw new Error("Please enter number");
+    else if ( a <= 20) return list[0];
+    else if ( a > 20 && a<=30) return list[1];
+    else if ( a > 30  && a<=40) return list[2];
+    else if ( a > 40 && a<=50) return list[3];
+    else if ( a >50 && a<=60) return list[4];
+    else{
+        return new Promise((resolve, reject) => {
+            setTimeout(()=> {
+                resolve(list[5]);
+            }, 5000);
+        });
+    }
+} */
+
+/* console.log("passed here 0");
+maslahatBering(25)
+   .then((data) => {
+    console.log(data);
+   }).catch( (err) => 
+      console.log("Error: ", err)
+   );
+console.log("passed here 1"); */
+
+/* async function  run () {
+    let javob = await maslahatBering(20);
+    console.log(javob);
+    javob = await maslahatBering(28);
+    console.log(javob);
+    javob = maslahatBering(35);
+    console.log(javob);
+    javob = maslahatBering(55);
+    console.log(javob);
+    
+}
+run(); */ 
