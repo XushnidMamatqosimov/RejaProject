a2138a102cff5c24ed9767bda979a9cbb922211f
Xushnid <xmamtqosimov@gmail.com>
2026-09-02 18:51:22 +0900

    fix: mogodb and app.js server.js modified

---
+++ app.js
@@ -0,0 +1,56 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+
+// MongoDB chaqirish;
+const db = require("./server").db();
+let user;
+
+/* fs.readFile("database/user.json", "utf-8", function(err, data) {
+    if (err) {
+        console.log("Xatolik yuz berdi: ", err);
+    } else {
+        user = JSON.parse(data);
+    }
+}); */
+
+// 1 - Kirish codelari
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Session code
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+
+app.post("/create-item", function(req, res) {
+    console.log(req.body);
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({ reja: new_reja },(err, data) => {
+         if (err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.end("successfully added");
+        }
+    });
+});
+
+app.get("/", function(req, res) { 
+    db.collection("plans")
+        .find()
+        .toArray((err, data) => {
+            if (err) {
+                console.log(err);
+                res.end("something went wrong");
+            } else {
+                console.log(data);
+                res.render("reja", {items: data});
+            }
+        });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,46 +1,26 @@
-console.log("Web serverni boshlash");
-
-const express = require("express");
-const app =  express();
-
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf-8", function(err, data){
+let db;
+const connectionString="mongodb+srv://xmamatqosimov_db_user:dTyfN7RH56ri4wta@cluster0.kigdam9.mongodb.net/";
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client)=> {
     if(err){
-        console.log("Xatolik yuz berdi: ", err);
-    } else {
-        user = JSON.parse(data);
+        console.log("error");
+    } 
+    db = client.db("Reja");
+    module.exports = {
+        db:function(){
+            return db
+        },
     }
-});
-
-
-
-// 1 - Kirish codelari
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// 2: Session code
-// 3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing code
-app.post("/create-item", function(req, res){
-    console.log(req.body);
-    res.send("Form ma'lumotlari qabul qilindi");
-});
-app.get("/", function(req, res){
-    res.render("reja");
-});
-app.get("/author", function(req, res){
-    res.render("author", {user: user});
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-    console.log("The server is running successfully on port: ", PORT, `https://localhost:${PORT}/`);
+    console.log("mongoDB connection succeed");
+    const app = require("./app");
+    const server = http.createServer(app);
+    let PORT = 3000;
+    server.listen(PORT, function() {
+    console.log("The server is running successfully on port: ", PORT,`http://localhost:${PORT}/` );
+     });
 })
+
+
+
