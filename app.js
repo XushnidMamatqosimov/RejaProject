a1c5da82da71431ff5d87288e3974041194984d0
Xushnid <xmamtqosimov@gmail.com>
2026-09-03 20:26:49 +0900

    feat: Frontend javaScripni qurish

--- app.js
+++ app.js
@@ -26,20 +26,21 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 
-app.post("/create-item", function(req, res) {
+app.post("/create-item", function (req, res) {
     console.log(req.body);
     const new_reja = req.body.reja;
-    db.collection("plans").insertOne({ reja: new_reja },(err, data) => {
-         if (err) {
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+        if (err) {
             console.log(err);
-            res.end("something went wrong");
+            res.status(500).json({ error: "something went wrong" });
         } else {
-            res.end("successfully added");
+            res.json({ _id: data.insertedId, reja: new_reja }); // <-- _id ham qo'shildi
         }
     });
+
 });
 
-app.get("/", function(req, res) { 
+app.get("/", function (req, res) {
     db.collection("plans")
         .find()
         .toArray((err, data) => {
@@ -48,9 +49,18 @@ app.get("/", function(req, res) {
                 res.end("something went wrong");
             } else {
                 console.log(data);
-                res.render("reja", {items: data});
+                res.render("reja", { items: data });
             }
         });
 });
 
+app.get("/author", function (req, res) {
+
+    const user = {
+        name: "Xushnid",
+        profession: "Developer"
+    };
+
+    res.render("author", { user });
+});
 module.exports = app;
